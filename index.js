3e00ac5d106396d52cd70c3a8a576695235eddfa
rmeghlan <rmeghlan@wdtablesystems.com>
2018-08-12 03:10:41 +0530

    Export store from the index.js file.

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import {createStore} from 'redux';
 import contactStore from './reducers';
 
-
+const store = createStore(contactStore);
+export default store;
 
 /**
  * @example: Following example states the way to dispatch and listen to state updates,
  * to be used by the consumer of this module.
  *
- const store = createStore(contactStore);
  const unscubscribe = store.subscribe(() => {
     console.log("Subscriber: ", store.getState());
 });
 
 
-store.dispatch(addContact({//obj goes here}));
+ store.dispatch(addContact({//obj goes here}));
 
 
-unscubscribe();
-*/
+ unscubscribe();
+ */
