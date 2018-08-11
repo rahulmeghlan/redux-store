ddd1d57b45eca35b9b67c98344d8c360cca272b5
rmeghlan <rmeghlan@wdtablesystems.com>
2018-08-12 01:12:44 +0530

    Fixed test cases to cover 100% test coverage.

--- src/reducers/filter.reducer.spec.js
+++ src/reducers/filter.reducer.spec.js
@@ -1,14 +1,22 @@
-import {FILTER_CONTACTS, applyFilter} from "../actions/filter.action";
+import {FILTER_CONTACTS} from "../actions/filter.action";
 import filters from './filters.reducer';
-
+const filter = {
+    firstName: 'xxxx',
+    phone: {
+        code: 'xx'
+    }
+};
 describe('Filters', function () {
-    const filter = {
-        firstName: 'xxxx',
-        phone: {
-            code: 'xx'
-        }
-    };
+
+    test('Return default state if no action provided', () => {
+        expect(filters({}, {})).toEqual({});
+    });
+
+    test('Return default state if wrong action provided', () => {
+        expect(filters({}, {type: 'NO_ACTION_TYPE', filter})).toEqual({});
+    });
+
     test('Selected filters get stored in the state', () => {
-       expect(filters({}, {type: FILTER_CONTACTS, filter})).toEqual(filter);
+        expect(filters({}, {type: FILTER_CONTACTS, filter})).toEqual(filter);
     });
 });
--- src/reducers/sort.reducer.spec.js
+++ src/reducers/sort.reducer.spec.js
@@ -2,6 +2,10 @@ import {SORT_CONTACTS} from "../actions/sort.action";
 import sort from './sort.reducer';
 
 describe('Sort ', function () {
+    test('should return default state if no action passed', () => {
+        expect(sort({}, {})).toEqual({});
+    });
+
     test('should return the sort obj passed', () => {
         const fieldOrder = {
             field: 'firstName',
